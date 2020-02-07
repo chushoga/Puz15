5d51d6dd4078dccaf22da6547bea1e41336e6962
chushoga <chushoga@gmail.com>
2020-02-07 15:59:01 +0900

    updated a few thiings

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -12,6 +12,9 @@ public class GameManager : MonoBehaviour
     LevelManager lm; // Level manager reference.
     bool gameOver = false; // Game over flag.
 
+    [Tooltip("Game Board")]
+    public GameObject GameBoard;
+
     [Tooltip("Puzzle Peices")]
     public List<GameObject> puzzlePeices;
 
@@ -21,40 +24,27 @@ public class GameManager : MonoBehaviour
         
         // Find and set the LevelManager object and set it to the reference.
         lm = GameObject.Find("LevelManager").gameObject.GetComponent<LevelManager>();
-        float padding = 1.15f;
-        int pSize = 4;
 
+        // Instantiate the Gamebord first then the peices.
+        GameObject board = Instantiate(GameBoard, new Vector3(0f, 0f, 0f), Quaternion.identity);
         // Create a parent for the puzzle peices.
-        GameObject puzzleCenter = new GameObject("PuzzleCenter");
+        GameObject puzzleCenter = new GameObject("Peices");
 
-        // Instantiate the puzzle pieces.
-        for (int i = 0; i < pSize; i++)
+        // Instantiate a game peice for each spawn point in the board object.
+        foreach (Transform t in board.transform)
         {
-            for (int j = 0; j < pSize; j++)
+            if(t.tag == "PeiceSpawn")
             {
-                if(i == 3 && j == 3){
-                    print("skip me" + i + "," + j);
-                } else
-                {   
-                    GameObject p = Instantiate(puzzlePeices[0], new Vector3(i * padding, 0f, -j * padding), Quaternion.identity);
-                    p.transform.parent = puzzleCenter.transform;
-                }
+                //GameObject p = Instantiate(puzzlePeices[0], new Vector3(t.transform.position.x, t.transform.position.y, t.transform.position.z), Quaternion.identity);
+                //p.transform.parent = puzzleCenter.transform; // For keeping the inspector clutter free. Just housekeeping.
             }
-
-            // Calculate the center position of the puzzle
-            float totalX = 0f;
-            float totalZ = 0f;
-            foreach(Transform go in puzzleCenter.transform)
-            {
-                totalX += go.transform.position.x;
-                totalZ += go.transform.position.z;
-            }
-            float centerX = totalX / puzzleCenter.transform.childCount;
-            float centerZ = totalZ / puzzleCenter.transform.childCount;
-
-            Camera.main.transform.position  = new Vector3(centerX, Camera.main.transform.position.y, centerZ);
         }
-
     }
 
+    // Game update
+    private void Update()
+    {
+        float speed = 100f;
+        Camera.main.transform.Rotate(new Vector3(0f,0f,1f) * Time.deltaTime * speed);
+    }
 }
