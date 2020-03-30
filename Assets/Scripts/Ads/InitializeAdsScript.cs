283bc4c5cd6a8f0fbeb91eb2ed2581cc6a81dba3
chushoga <chushoga@gmail.com>
2020-03-30 17:38:57 +0900

    fixed fadeout and tried ads

---
+++ Assets/Scripts/Ads/InitializeAdsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour
+{
+
+    public string gameId = "3530949";
+    public string placementId = "MainMenu";
+    public bool testMode = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+        StartCoroutine(ShowBannerWhenReady());
+        print("test1");
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        print("test2");
+        while (!Advertisement.IsReady(placementId))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        
+        Advertisement.Banner.Show(placementId);
+    }
+
+}
